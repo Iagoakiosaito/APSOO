9ce8d04824970949d8b686606b2467261ac51a78
Iago Saito <iagoosaito@gmail.com>
2021-05-28 16:24:23 -0400

    Adicionado as classes Venda e Carrinho

--- TrabalhoAPSOO/src/Classes/Carrinho.java
+++ TrabalhoAPSOO/src/Classes/Carrinho.java
@@ -2,4 +2,48 @@ package Classes;
 
 public class Carrinho {
 
+	private int qualidadeProdutos;
+	private double valorVenda;
+	private String formaPagamento;
+	
+	
+	public Carrinho(int qualidadeProdutos, double valorVenda, String formaPagamento) {
+		super();
+		this.qualidadeProdutos = qualidadeProdutos;
+		this.valorVenda = valorVenda;
+		this.formaPagamento = formaPagamento;
+	}
+
+
+	public int getQualidadeProdutos() {
+		return qualidadeProdutos;
+	}
+
+
+	public void setQualidadeProdutos(int qualidadeProdutos) {
+		this.qualidadeProdutos = qualidadeProdutos;
+	}
+
+
+	public double getValorVenda() {
+		return valorVenda;
+	}
+
+
+	public void setValorVenda(double valorVenda) {
+		this.valorVenda = valorVenda;
+	}
+
+
+	public String getFormaPagamento() {
+		return formaPagamento;
+	}
+
+
+	public void setFormaPagamento(String formaPagamento) {
+		this.formaPagamento = formaPagamento;
+	}
+	
+	
+	
 }
--- TrabalhoAPSOO/src/Classes/Funcionario.java
+++ TrabalhoAPSOO/src/Classes/Funcionario.java
@@ -1,5 +1,53 @@
 package Classes;
 
-public class Funcionario {
+
+public class Funcionario extends Pessoa {
+
+	private int codigoFuncionario;
+	private double salario;
+	private String cargo;
+	private String dataAdmissao;
+
+	public Funcionario(String nome, String cpf, String endereco, String telefone, int codigoFuncionario, double salario,
+			String cargo, String dataAdmissao) {
+		super(nome, cpf, endereco, telefone);
+		this.codigoFuncionario = codigoFuncionario;
+		this.salario = salario;
+		this.cargo = cargo;
+		this.dataAdmissao = dataAdmissao;
+	}
+
+	public int getCodigoFuncionario() {
+		return codigoFuncionario;
+	}
+
+	public void setCodigoFuncionario(int codigoFuncionario) {
+		this.codigoFuncionario = codigoFuncionario;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public String getDataAdmissao() {
+		return dataAdmissao;
+	}
+
+	public void setDataAdmissao(String dataAdmissao) {
+		this.dataAdmissao = dataAdmissao;
+	}
+
 
 }
