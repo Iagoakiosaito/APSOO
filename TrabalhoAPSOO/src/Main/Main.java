38547f1fb1aca80e27ad1399051d339d3ca95d73
Iago_saito <gokuiago@gmail.com>
2021-05-28 15:42:17 -0300

    Commit para teste

---
+++ TrabalhoAPSOO/src/Main/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Main;
+
+/**
+ * @author Iago
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Teste");
+	}
+
+}
