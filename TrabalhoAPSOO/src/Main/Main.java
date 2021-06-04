695f8a1867755e75800159f979ef01dacab1246d
Lucas Volpe <lucasvolpe1@hotmail.com>
2021-06-04 16:14:33 -0400

    Adição driver bd

--- TrabalhoAPSOO/src/Main/Main.java
+++ TrabalhoAPSOO/src/Main/Main.java
@@ -3,8 +3,11 @@
  */
 package Main;
 
-import Models.Carrinho;
-import Models.Venda;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import database.ConnectionFactory;
 
 public class Main {
 
@@ -12,44 +15,10 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
-		while(true) {
-			
-			//scanner op
-			System.out.println("[ 1 ] - iniciar nova venda");
-			
-			switch(op) {
-				case 1:
-					Venda venda = new Venda();
-					java.time.LocalDate.now();
-					java.time.LocalTime.now();
-					Carrinho carrinho = new carrinho();
-					
-					while(true) {
-						codProduto = scaner
-						produto = produtocontroller.findbycod(codprodu)
-						carrinho.addProduto(produto);
-						
-						break
-					}
-					
-					
-					valortola = carrinho.calcularValorTotal()
-					system.out.println("Valor total da compra: " + valortotal)
-					
-					
-					//metodos de pagamento
-					
-					carrinho.addFormaPagamento(null);
-					venda.addCarrinho(carrinho);
-					
-					//bd
-					
-					
-					//
-					carrinho.descontarProdutosEstoque();
-			}
-			
-		}
+		
+		Connection con = new ConnectionFactory().getConnection();
+		System.out.println("Conectou chefe, confia");
+        
 		
 		
 	}
